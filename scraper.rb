cd2b38a6ecf4a17fbe6a2d6c26ed077565f7fe39
kevinlu <kevinlu.rea@gmail.com>
2020-04-14 19:09:35 -0400

    Store listings info to csv

--- scraper.rb
+++ scraper.rb
@@ -16,29 +16,38 @@ def scraper(city)
     rangeStart = 0
     # last page will be the remainder 
     rangeEnd = total_count - (total_count % list_count)
-    info_arr = []
-    while rangeStart < rangeEnd
-        pagination_url = "https://#{city.downcase}.craigslist.org/search/apa?s=#{rangeStart}&availabilityMode=0&max_bathrooms=2&max_bedrooms=3&min_bathrooms=2&min_bedrooms=3"
-        unparsed_current_page = HTTParty.get(pagination_url)
-        parsed_current = Nokogiri::HTML(unparsed_current_page)
-        listings.each.with_index do |list, i|
-            title = parsed_current.css('a.result-title')[i]
-            rent = parsed_current.css('span.result-price')[i]
-            address = parsed_current.css('span.result-hood')[i]
+
+
+    CSV.open('cl_listings.csv', 'wb') do |csv|
+        csv << ["Title", "Rent", "Address", "URL"]
+        while rangeStart < rangeEnd
+            pagination_url = "https://#{city.downcase}.craigslist.org/search/apa?s=#{rangeStart}&availabilityMode=0&max_bathrooms=2&max_bedrooms=3&min_bathrooms=2&min_bedrooms=3"
+            unparsed_current_page = HTTParty.get(pagination_url)
+            parsed_current = Nokogiri::HTML(unparsed_current_page)
+            listings.each.with_index do |list, i|
+                title = parsed_current.css('a.result-title')[i]
+                rent = parsed_current.css('span.result-price')[i]
+                address = parsed_current.css('span.result-hood')[i]
+           
+                list_info = [
+                    title ? title.text : "",
+                    rent ? rent.text : "",
+                    address ? address.text.tr('()', '').strip : "",
+                    title ? title.attributes["href"].value : ""
+                ]
+                
+                csv << list_info
         
-            list_info = {
-                title: title ? title.text : "",
-                rent:  rent ? rent.text : "",
-                address: address ? address.text : "",
-                url: title ? title.attributes["href"].value : ""
-            }    
-            info_arr << list_info
+            end
+            rangeStart += list_count
         end
-        rangeStart += list_count
-        
     end
-   ap info_arr
+
 end
+
+
+
 
 scraper("atlanta")
 
+  
