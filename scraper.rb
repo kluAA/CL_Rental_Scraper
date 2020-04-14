560e17c7fbcc1b680fb524e1ce349a9dbaf38e7b
kevinlu <kevinlu.rea@gmail.com>
2020-04-14 16:10:35 -0400

    Add pagination logic

--- scraper.rb
+++ scraper.rb
@@ -2,29 +2,43 @@ require 'nokogiri'
 require 'httparty'
 require 'byebug'
 require 'awesome_print'
+require 'csv'
 
 def scraper(city)
-    url = "https://#{city.downcase}.craigslist.org/search/apa?min_bedrooms=3&max_bedrooms=3&min_bathrooms=2&max_bathrooms=2&availabilityMode=0&sale_date=all+dates"
+    url = "https://#{city.downcase}.craigslist.org/search/apa?availabilityMode=0&max_bathrooms=2&max_bedrooms=3&min_bathrooms=2&min_bedrooms=3"
     unparsed_page = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(unparsed_page)
 
-    # totalcount = parsed_page.css('span.totalcount')[0].text.to_i
     # range = parsed_page.css('span.rangTo')[0].text.to_i
     listings = parsed_page.css('a.result-title')
-
+    list_count = listings.count
+    total_count = parsed_page.css('span.totalcount')[0].text.to_i
+    rangeStart = 0
+    # last page will be the remainder 
+    rangeEnd = total_count - (total_count % list_count)
     info_arr = []
-    listings.each.with_index do |list, i|
-        title = parsed_page.css('a.result-title')[i]
-        rent = parsed_page.css('span.result-price')[i]
-        address = parsed_page.css('span.result-hood')[i]
-        list_info = {
-            title: title ? title.text : "",
-            rent:  rent ? rent.text : "",
-            address: address ? address.text : ""
-        }
-        info_arr << list_info
+    while rangeStart < rangeEnd
+        pagination_url = "https://#{city.downcase}.craigslist.org/search/apa?s=#{rangeStart}&availabilityMode=0&max_bathrooms=2&max_bedrooms=3&min_bathrooms=2&min_bedrooms=3"
+        unparsed_current_page = HTTParty.get(pagination_url)
+        parsed_current = Nokogiri::HTML(unparsed_current_page)
+        listings.each.with_index do |list, i|
+            title = parsed_current.css('a.result-title')[i]
+            rent = parsed_current.css('span.result-price')[i]
+            address = parsed_current.css('span.result-hood')[i]
+        
+            list_info = {
+                title: title ? title.text : "",
+                rent:  rent ? rent.text : "",
+                address: address ? address.text : "",
+                url: title ? title.attributes["href"].value : ""
+            }    
+            info_arr << list_info
+        end
+        rangeStart += list_count
+        
     end
    ap info_arr
 end
 
 scraper("atlanta")
+
