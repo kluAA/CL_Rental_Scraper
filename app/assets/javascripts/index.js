7845f4e573e9ce7bab53ce9ecbf5d27c580e9061
kevinlu <kevinlu.rea@gmail.com>
2020-04-15 06:14:44 -0400

    Style scraper app

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", e => {
-    const root = document.getElementById("root");
     const input = document.getElementById("city-input");
     const button = document.getElementById("grab-link");
     const linkLocation = document.getElementById("link");
+    const message = document.getElementById("message");
     button.addEventListener('click', e => {
         e.preventDefault();
+        message.innerText = "Download: Generating link..."
         grabCSV(input.value)
             .then(csv => {
                 const link = document.createElement("a");
                 const encoded = encodeURI(csv);
                 const date = new Date().toISOString().substring(0,10);
-                console.log(encoded)
+                message.innerText = "Download: ";
                 link.setAttribute("href", 'data:text/csv;charset=UTF-8,' + encoded);
                 link.setAttribute("download", `${date}_${input.value}_3_bedroom_2_bathroom.csv`);
-                link.text = "test";
+                link.text = `${date}_${input.value}_3_bedroom_2_bathroom.csv`;
                 linkLocation.append(link);
-                
             })
     })
 
